0aebc059ee768c408248a15528a111b6b29a7db2
Gulevskii Ivan <ithirteengw@gmail.com>
2022-06-03 21:18:02 +0700

    Сделал анимации движения + фулл первую локацию

--- HItsGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ HItsGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed = 5f;
-    public Vector2 position;
+    private Rigidbody2D rb;
+    public float speed = 4f;
+    private Vector2 position;
+    private Animator animator;
+
+    private void Start()
+    {
+       animator = GetComponent<Animator>();
+       rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         position.x = Input.GetAxisRaw("Horizontal");
         position.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal", position.x);
+        animator.SetFloat("Vertical", position.y);
+        animator.SetFloat("Speed", position.sqrMagnitude);
     }
 
     private void FixedUpdate()
