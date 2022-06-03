bef00503ff6960390e607dbaa4c10a1fb1fff260
Gulevskii Ivan <ithirteengw@gmail.com>
2022-06-03 22:32:14 +0700

    Добавил переход между комнатами и корректные позиции после них

--- HItsGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ HItsGame/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,27 +7,38 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed = 4f;
-    private Vector2 position;
+    private Vector2 movement;
     private Animator animator;
+    public SavedPosition position;
 
     private void Start()
     {
        animator = GetComponent<Animator>();
        rb = GetComponent<Rigidbody2D>();
+       transform.position = position.initialValue;
     }
 
     void Update()
     {
-        position.x = Input.GetAxisRaw("Horizontal");
-        position.y = Input.GetAxisRaw("Vertical");
         
-        animator.SetFloat("Horizontal", position.x);
-        animator.SetFloat("Vertical", position.y);
-        animator.SetFloat("Speed", position.sqrMagnitude);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + position * (speed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + movement * (speed * Time.fixedDeltaTime));
+    }
+
+    private void OnApplicationQuit()
+    {
+        Vector3 update;
+        update.x = 6.477f;
+        update.y = -1.3f;
+        update.z = 0;
+        position.initialValue = update;
     }
 }
