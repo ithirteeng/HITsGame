fb49e0aabfa1ba4c9463447eaaeafb7de3083abf
Gulevskii Ivan <ithirteengw@gmail.com>
2022-06-21 21:13:46 +0700

    again

---
+++ HItsGame/Assets/Scripts/GardenScripts/MinigameAppearance.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinigameAppearance : MonoBehaviour
+{
+    public string minigameScene;
+    public Camera camera;
+    public GameObject player;
+    public GameObject pressECanvas;
+    private bool isInTirgger;
+
+    private void Start()
+    {
+        PlayerAppearance.Init(player, camera);
+    }
+
+    private void Update()
+    {
+        if (isInTirgger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadSceneAsync(minigameScene, LoadSceneMode.Additive);
+                player.SetActive(false);
+                pressECanvas.SetActive(false);
+                camera.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        isInTirgger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isInTirgger = false;
+    }
+}
