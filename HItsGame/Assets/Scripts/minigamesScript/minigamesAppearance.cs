2395d285770b42ba7826cac3fc261ff647236fa3
Gulevskii Ivan <ithirteengw@gmail.com>
2022-06-24 13:44:58 +0700

    DONE

--- HItsGame/Assets/Scripts/minigamesScript/minigamesAppearance.cs
+++ HItsGame/Assets/Scripts/minigamesScript/minigamesAppearance.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class minigamesAppearance : MonoBehaviour
 {
@@ -12,9 +14,14 @@ public class minigamesAppearance : MonoBehaviour
     public GameObject player;
     public GameObject pressECanvas;
     private bool isInTirgger;
+    [FormerlySerializedAs("mesageCanvas")] public GameObject messageCanvas;
+    public TextMeshProUGUI text;
+    public string game;
 
     private void Start()
     {
+        messageCanvas.SetActive(false);
+        pressECanvas.SetActive(false);
         PlayerAppearance.Init(player, camera);
         
     }
@@ -36,11 +43,14 @@ public class minigamesAppearance : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         isInTirgger = true;
+        messageCanvas.SetActive(true);
+        text.text = game;
         pressECanvas.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        messageCanvas.SetActive(false);
         pressECanvas.SetActive(false);
         isInTirgger = false;
     }
